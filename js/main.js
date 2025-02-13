de2751d7d002adfaf281f01105c913cb2dcd22a0
taerinida <ekqls0873@daum.net>
2025-02-13 20:36:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$('.home').on('click', function(){
+   let offset=$(".page1").offset(); 
+   $('html, body').animate({scrollTop:offset.top},500);   
+   return false;
+})
+$('.pf').on('click', function(){
+   let offset=$(".page2").offset(); 
+   $('html, body').animate({scrollTop:offset.top},500);   
+   return false;
+})
+$('.p3').on('click', function(){
+   let offset=$(".page3").offset(); 
+   $('html, body').animate({scrollTop:offset.top},500);   
+   return false;
+})
+$('.wiki').on('click', function(){
+   let offset=$(".page4").offset(); 
+   $('html, body').animate({scrollTop:offset.top},500);   
+   return false;
+})
+$('.gm').on('click', function(){
+   let offset=$(".page6").offset(); 
+   $('html, body').animate({scrollTop:offset.top},500);   
+   return false;
+})
+$('.art').on('click', function(){
+   let offset=$(".page8").offset(); 
+   $('html, body').animate({scrollTop:offset.top},500);   
+   return false;
+})
+$('.pledis').on('click', function(){
+   let offset=$(".page10").offset(); 
+   $('html, body').animate({scrollTop:offset.top},500);   
+   return false;
+})
